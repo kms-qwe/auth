aad4cb0cc22f1328c21f2e2d71a3d46b6b6ec622
kms-qwe <mishakr8987@gmail.com>
2024-10-07 17:00:53 +0300

    add mock implementation of grpc serv and client

--- app/cmd/grpc_client/main.go
+++ app/cmd/grpc_client/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"time"
 
+	desc "github.com/kms-qwe/microservices_course_auth/app/pkg/user_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	address = "localhost:9001"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to server: %v", err)
+	}
+	defer conn.Close()
+
+	client := desc.NewUserV1Client(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	response, err := client.Get(ctx, &desc.GetRequest{Id: 0})
+	if err != nil {
+		log.Fatalf("failed to get user by id: %v", err)
+	}
+
+	log.Printf("user info: %#v\n", response)
 }
--- app/cmd/grpc_server/main.go
+++ app/cmd/grpc_server/main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	desc "github.com/kms-qwe/microservices_course_auth/app/pkg/user_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+const grpcPort = 9001
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("get create request: %#v:\n", req)
+	return &desc.CreateResponse{Id: 0}, nil
+}
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("get gete request: %#v:", req)
+	return &desc.GetResponse{User: &desc.User{}}, nil
+}
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("get update request: %#v\n", req)
+	return &emptypb.Empty{}, nil
+}
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("get delete request: %#v\n", req)
+	return &emptypb.Empty{}, nil
+}
 func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", grpcPort))
+	if err != nil {
+		log.Fatalf("falied to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 
 }
