824db75f68e3a5e5d4912e17e42e237c5465ed91
kms-qwe <mishakr8987@gmail.com>
2024-10-26 21:13:11 +0300

    fix migration.sh

--- build/migration.sh
+++ build/migration.sh
@@ -1,3 +1,3 @@
-export MIGRATION_DSN="host=$DB_HOST port=$PG_INNER_PORT dbname=$PG_DATABASE_NAME user=$PG_USER password=$PG_PASSWORD sslmode=disable"
+export MIGRATION_DSN="host=$DB_HOST port=$PG_INNER_PORT dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=disable"
 
 sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v && echo "SUCCESS"
